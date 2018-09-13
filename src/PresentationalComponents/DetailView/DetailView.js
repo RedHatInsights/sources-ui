c3158e62d69568e31a79609cac7f09e8a4a0204c
Martin Povolny <mpovolny@redhat.com>
2018-09-13 22:23:37 +0200

    Put details into a Grid.

--- src/PresentationalComponents/DetailView/DetailView.js
+++ src/PresentationalComponents/DetailView/DetailView.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import { Button } from '@patternfly/react-core';
+import { Card, Grid, GridItem, Button } from '@patternfly/react-core';
 import { Donut } from '@red-hat-insights/insights-frontend-components';
+//import { Pie } from '@red-hat-insights/insights-frontend-components';
 
 class DetailView extends Component {
     render() {
         return (
-          <div>
-              <Donut withLegend identifier='orech' values={[['Red Hat', 100], ['Google', 10]]}/>
-              <dl>
-                <dt>IP Address</dt><dd>192.168.2.1</dd>
-                <dt>Location</dt><dd>Brn</dd>
-                <dt>Status</dt><dd>Ready for Fredi</dd>
-                <dt>Licence</dt><dd>Apache Licence 2.0</dd>
-              </dl>
-              <div>
-                <Button variant='primary'>Tag</Button>
-                <Button variant='primary'>Animal</Button>
-                <Button variant='primary'>Foobar</Button>
-                <Button variant='primary'>Another Tag</Button>
-              </div>
-          </div>
+          <Grid>
+              <GridItem sm={6} md={4} lg={4} xl={4}>
+                  <Donut withLegend identifier='orech' values={[['Red Hat', 100], ['Google', 10]]}/>
+              </GridItem>
+              <GridItem sm={6} md={4} lg={4} xl={4}>
+                  <dl>
+                    <dt>IP Address</dt><dd>192.168.2.1</dd>
+                    <dt>Location</dt><dd>Brno</dd>
+                    <dt>Status</dt><dd>Ready for Fredie</dd>
+                    <dt>Licence</dt><dd>Apache Licence 2.0</dd>
+                  </dl>
+              </GridItem>
+              <GridItem sm={6} md={4} lg={4} xl={3}>
+                  <Button variant='primary'>Tag</Button>
+                  <Button variant='primary'>Animal</Button>
+                  <Button variant='primary'>Foobar</Button>
+                  <Button variant='primary'>Another Tag</Button>
+              </GridItem>
+          </Grid>
         )
     }
 }
