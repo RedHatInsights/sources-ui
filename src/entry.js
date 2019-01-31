afed93b6cb96b028c340fe2cd2437c1081cfdf63
Martin Povolny <mpovolny@redhat.com>
2019-01-31 10:44:04 +0100

    Fix routes.

--- src/entry.js
+++ src/entry.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import promiseMiddleware from 'redux-promise-middleware';
 import App from './App';
 
-function todos(state = [], action) {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return state.concat([action.text]);
-        default:
-            return state;
-    }
-}
-
-const store = createStore(todos, applyMiddleware(promiseMiddleware()));
+// exposes webpack variable RELEASE
+/*global RELEASE:true*/
+/*eslint no-undef: "error"*/
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
+    <Provider store={App.getRegistry().getStore()}>
+        <Router basename={ `/${RELEASE}/platform/topological-inventory` }>
             <App />
         </Router>
     </Provider>,
