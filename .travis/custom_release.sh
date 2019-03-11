2c034d09f2bf6bdc87da53076d9cc4aae0c31c1e
Jason Frey <jfrey@redhat.com>
2019-03-11 14:56:00 -0400

    Deploy to beta as well as stable

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,12 +2,11 @@
 
 NODE_ENV=production npm run build
 
-# If current dev branch is master, push to build repo ci-stable
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    .travis/release.sh "ci-stable"
-fi
-
-# If current dev branch is deployment branch, push to build repo
-if [[ "${TRAVIS_BRANCH}" = "ci-beta"  || "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+    .travis/release.sh "ci-beta"
+    .travis/promote.sh "qa-beta"
+    .travis/promote.sh "ci-stable"
+    .travis/promote.sh "qa-stable"
+elif [[ "${TRAVIS_BRANCH}" = "ci-stable"  || "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
