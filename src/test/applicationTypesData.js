f33a9921d97e75eb0cb754e657e111f2a4140c28
Martin Povolny <mpovolny@redhat.com>
2019-05-20 10:21:22 +0200

    Update specs to match the new API calls.

---
+++ src/test/applicationTypesData.js
@@ -0,0 +1,10 @@
+export const applicationTypesData =
+{
+    meta: { count: 3, limit: 100, offset: 0 },
+    links: { first: '/api/v1.0/application_types?offset=0', last: '/api/v1.0/application_types?offset=0' },
+    data: [
+        { created_at: '2019-04-30T10:56:21Z', display_name: 'Catalog', id: '1', name: '/insights/platform/catalog', updated_at: '2019-04-30T10:56:21Z' },
+        { created_at: '2019-04-30T10:56:21Z', display_name: 'Cost Management', id: '2', name: '/insights/platform/cost-management', updated_at: '2019-04-30T10:56:21Z' },
+        { created_at: '2019-04-30T10:56:21Z', display_name: 'Topological Inventory', id: '3', name: '/insights/platform/topological-inventory', updated_at: '2019-04-30T10:56:21Z' }
+    ]
+};
