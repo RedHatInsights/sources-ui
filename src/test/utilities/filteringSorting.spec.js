5acbd5ca6a19abb8811f356434a3263502b3fe73
Richard Vsiansky <r.vsia@seznam.cz>
2019-10-25 09:47:03 +0200

    Fix filtering

--- src/test/utilities/filteringSorting.spec.js
+++ src/test/utilities/filteringSorting.spec.js
@@ -2,12 +2,19 @@ import { sortByCompare, filterByValue, paginate, prepareEntities } from '../../U
 
 describe('filteringSorting utilities', () => {
     let testingArray;
+    let testingArrayWithApps;
 
     beforeEach(() => {
         testingArray = [
             { name: 'Bob', id: 8 },
             { name: 'Cecil', id: 1 },
             { name: 'Alois', id: 41 }
+        ];
+
+        testingArrayWithApps = [
+            { apps: ['1', '2'] },
+            { apps: [] },
+            { apps: ['3'] }
         ];
     });
 
@@ -26,6 +33,66 @@ describe('filteringSorting utilities', () => {
                 { name: 'Bob', id: 8 },
                 { name: 'Cecil', id: 1 }
             ]);
+        });
+
+        it('compares array by length ASC', () => {
+            expect(testingArrayWithApps.sort(sortByCompare('apps', 'asc'))).toEqual([
+                { apps: [] },
+                { apps: ['3'] },
+                { apps: ['1', '2'] }
+            ]);
+        });
+
+        it('compares array by length DESC', () => {
+            expect(testingArrayWithApps.sort(sortByCompare('apps', 'desc'))).toEqual([
+                { apps: ['1', '2'] },
+                { apps: ['3'] },
+                { apps: [] }
+            ]);
+        });
+
+        it('do not compare different types', () => {
+            testingArray = [
+                { apps: 'app' },
+                { apps: ['1'] }
+            ];
+
+            expect(testingArray.sort(sortByCompare('apps', 'desc'))).toEqual(testingArray);
+            expect(testingArray.sort(sortByCompare('apps', 'asc'))).toEqual(testingArray);
+        });
+
+        describe('source_type_id', () => {
+            const FIRST_SOURCE_ID = '300';
+            const NEXT_SOURCE_ID = '1';
+            const LAST_SOURCE_ID = '15';
+
+            const SOURCE_TYPES = [
+                { id: NEXT_SOURCE_ID, product_name: 'B' },
+                { id: LAST_SOURCE_ID, product_name: 'C' },
+                { id: FIRST_SOURCE_ID, product_name: 'A' }
+            ];
+
+            const testingArrayTypes = [
+                { source_type_id: NEXT_SOURCE_ID },
+                { source_type_id: LAST_SOURCE_ID },
+                { source_type_id: FIRST_SOURCE_ID }
+            ];
+
+            it('sort ASC', () => {
+                expect(testingArrayTypes.sort(sortByCompare('source_type_id', 'asc', { sourceTypes: SOURCE_TYPES }))).toEqual([
+                    { source_type_id: FIRST_SOURCE_ID },
+                    { source_type_id: NEXT_SOURCE_ID },
+                    { source_type_id: LAST_SOURCE_ID }
+                ]);
+            });
+
+            it('sort DESC', () => {
+                expect(testingArrayTypes.sort(sortByCompare('source_type_id', 'desc', { sourceTypes: SOURCE_TYPES }))).toEqual([
+                    { source_type_id: LAST_SOURCE_ID },
+                    { source_type_id: NEXT_SOURCE_ID },
+                    { source_type_id: FIRST_SOURCE_ID }
+                ]);
+            });
         });
     });
 
