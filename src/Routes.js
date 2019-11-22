03c75eee78298ed5870844fa44d1354deaa8b815
Richard Vsiansky <r.vsia@seznam.cz>
2019-11-22 13:24:58 +0100

    Update routes to new default

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import some from 'lodash/some';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import ContentLoader from 'react-content-loader';
 
 const SourcesPage = lazy(() => import('./pages/SourcesPage'));
@@ -22,30 +20,19 @@ export const paths = {
     sourceManageApps: '/manage_apps/:id'
 };
 
-const Routes = (props) => {
-    const path = props.location.pathname;
+const Routes = () =>  (
+    <div className='pf-c-page__main pf-l-page__main'>
+        <Suspense fallback={<Loader/>}>
+            <Switch>
+                <Route exact path={paths.sources} component={SourcesPage} />
+                <Route exact path={paths.sourcesNew} component={SourcesPage} />
+                <Route path={paths.sourcesEdit} component={SourcesPage} />
+                <Route path={paths.sourcesRemove} component={SourcesPage} />
+                <Route path={paths.sourceManageApps} component={SourcesPage} />
+                <Route render={() => <Redirect to={paths.sources} />} />
+            </Switch>
+        </Suspense>
+    </div>
+);
 
-    return (
-        <div className='pf-c-page__main pf-l-page__main'>
-            <Suspense fallback={<Loader/>}>
-                <Switch>
-                    <Route exact path={paths.sources} component={SourcesPage} />
-                    <Route exact path={paths.sourcesNew} component={SourcesPage} />
-                    <Route path={paths.sourcesEdit} component={SourcesPage} />
-                    <Route path={paths.sourcesRemove} component={SourcesPage} />
-                    <Route path={paths.sourceManageApps} component={SourcesPage} />
-                    <Route render={() => some(paths, p => p === path) ? null : (<Redirect to={paths.providers} />)} />
-                </Switch>
-            </Suspense>
-        </div>
-    );
-};
-
-Routes.propTypes = {
-    childProps: PropTypes.any.isRequired,
-    location: PropTypes.shape({
-        pathname: PropTypes.string.isRequired
-    }).isRequired
-};
-
-export default withRouter(Routes);
+export default Routes;
