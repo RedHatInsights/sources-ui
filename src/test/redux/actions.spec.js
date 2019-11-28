ee5d7db2f22af4d8fbee19225fc9f1166dd8757a
Richard Vsiansky <r.vsia@seznam.cz>
2019-11-28 15:38:11 +0100

    Add test for updated actions

--- src/test/redux/actions.spec.js
+++ src/test/redux/actions.spec.js
@@ -1,10 +1,49 @@
-import { undoAddSource, clearAddSource, removeMessage, updateSource } from '../../redux/actions/providers';
-import { UNDO_ADD_SOURCE, CLEAR_ADD_SOURCE } from '../../redux/action-types-providers';
+import {
+    undoAddSource,
+    clearAddSource,
+    removeMessage,
+    updateSource,
+    addHiddenSource,
+    pageAndSize,
+    loadEntities,
+    sortEntities,
+    removeSource
+} from '../../redux/actions/providers';
+import {
+    UNDO_ADD_SOURCE,
+    CLEAR_ADD_SOURCE,
+    ADD_HIDDEN_SOURCE,
+    PAGE_AND_SIZE,
+    ACTION_TYPES,
+    SET_COUNT,
+    SORT_ENTITIES
+} from '../../redux/action-types-providers';
 import { REMOVE_NOTIFICATION, ADD_NOTIFICATION } from '@redhat-cloud-services/frontend-components-notifications';
 import * as updateSourceApi from '../../api/doUpdateSource';
+import * as api from '../../api/entities';
 
 describe('redux actions', () => {
-    const dispatch = (x) => x;
+    let dispatch;
+
+    beforeEach(() => {
+        dispatch = jest.fn().mockImplementation((x) => x);
+    });
+
+    afterEach(() => {
+        dispatch.mockReset();
+    });
+
+    it('undoAddSource creates an object', () => {
+        const SOURCE = { name: 'Stuart' };
+        expect(addHiddenSource(SOURCE)).toEqual(
+            expect.objectContaining({
+                type: ADD_HIDDEN_SOURCE,
+                payload: {
+                    source: SOURCE
+                }
+            })
+        );
+    });
 
     it('undoAddSource creates an object', () => {
         const VALUES = { name: 'Stuart' };
@@ -73,6 +112,195 @@ describe('redux actions', () => {
                     payload: ERROR
                 })
             );
+        });
+    });
+
+    describe('page and size', () => {
+        it('call dispatch with changing size and loading values', async () => {
+            const PAGE = 10;
+            const SIZE = 15;
+
+            await pageAndSize(PAGE, SIZE)(dispatch);
+
+            expect(dispatch.mock.calls).toHaveLength(2);
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: PAGE_AND_SIZE,
+                payload: { page: PAGE, size: SIZE }
+            });
+            expect(dispatch.mock.calls[1][0]).toEqual(expect.any(Function));
+        });
+    });
+
+    describe('sort entities', () => {
+        it('call dispatch with changing column and direction', async () => {
+            const COLUMN = 'type_id';
+            const DIRECTION = 'desc';
+
+            await sortEntities(COLUMN, DIRECTION)(dispatch);
+
+            expect(dispatch.mock.calls).toHaveLength(2);
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: SORT_ENTITIES,
+                payload: { column: COLUMN, direction: DIRECTION }
+            });
+            expect(dispatch.mock.calls[1][0]).toEqual(expect.any(Function));
+        });
+    });
+
+    describe('loadEntities', () => {
+        const count = 15124;
+        const sources = [{ aa: 'bb' }, { vv: 'ee' }];
+
+        const [pageSize, pageNumber, sortBy, sortDirection, filterValue] = [15, 10, 'name', 'desc', 'pepa'];
+
+        const getState = () => ({
+            providers: {
+                pageSize, pageNumber, sortBy, sortDirection, filterValue
+            }
+        });
+
+        beforeEach(() => {
+            api.doLoadEntities = jest.fn().mockImplementation(() => Promise.resolve({ sources }));
+            api.doLoadCountOfSources = jest.fn().mockImplementation(() => Promise.resolve(
+                { meta: { count } }
+            ));
+        });
+
+        it('loads entities', async () => {
+            await loadEntities()(dispatch, getState);
+
+            expect(dispatch.mock.calls).toHaveLength(3);
+
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_PENDING,
+                options: undefined
+            });
+            expect(dispatch.mock.calls[1][0]).toEqual({
+                type: SET_COUNT,
+                payload: { count }
+            });
+            expect(dispatch.mock.calls[2][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_FULFILLED,
+                payload: sources,
+                options: undefined
+            });
+        });
+
+        it('passes options to fulfilled', async () => {
+            const options = { custom: 'custom' };
+
+            await loadEntities(options)(dispatch, getState);
+
+            expect(dispatch.mock.calls).toHaveLength(3);
+
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_PENDING,
+                options: undefined
+            });
+            expect(dispatch.mock.calls[1][0]).toEqual({
+                type: SET_COUNT,
+                payload: { count }
+            });
+            expect(dispatch.mock.calls[2][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_FULFILLED,
+                payload: sources,
+                options
+            });
+        });
+
+        it('passes options to pending', async () => {
+            const options = { custom: 'custom' };
+
+            await loadEntities({}, options)(dispatch, getState);
+
+            expect(dispatch.mock.calls).toHaveLength(3);
+
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_PENDING,
+                options
+            });
+            expect(dispatch.mock.calls[1][0]).toEqual({
+                type: SET_COUNT,
+                payload: { count }
+            });
+            expect(dispatch.mock.calls[2][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_FULFILLED,
+                payload: sources,
+                options: {}
+            });
+        });
+
+        it('handles failure', async () => {
+            const ERROR_DETAIL = 'aaa';
+            const ERROR = { detail: ERROR_DETAIL };
+
+            api.doLoadEntities = jest.fn().mockImplementation(() => Promise.reject(ERROR));
+
+            await loadEntities()(dispatch, getState);
+
+            expect(dispatch.mock.calls).toHaveLength(3);
+
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_PENDING,
+                options: undefined
+            });
+            expect(dispatch.mock.calls[2][0]).toEqual({
+                type: ACTION_TYPES.LOAD_ENTITIES_REJECTED,
+                payload: { error: { detail: ERROR_DETAIL, title: expect.any(String) } }
+            });
+        });
+    });
+
+    describe('doRemoveSource', () => {
+        const sourceId = '12132145';
+        const title = 'Some title here';
+
+        it('loads entities', async () => {
+            api.doRemoveSource = jest.fn().mockImplementation(() => Promise.resolve('OK'));
+
+            await removeSource(sourceId, title)(dispatch);
+
+            expect(dispatch.mock.calls).toHaveLength(4);
+
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: ACTION_TYPES.REMOVE_SOURCE_PENDING,
+                meta: {
+                    sourceId
+                }
+            });
+
+            expect(dispatch.mock.calls[1][0]).toEqual(expect.any(Function));
+
+            expect(dispatch.mock.calls[2][0]).toEqual({
+                type: ACTION_TYPES.REMOVE_SOURCE_FULFILLED,
+                meta: {
+                    sourceId
+                }
+            });
+
+            expect(dispatch.mock.calls[3][0]).toEqual(expect.any(Function));
+        });
+
+        it('handle failure', async () => {
+            api.doRemoveSource = jest.fn().mockImplementation(() => Promise.reject('OK'));
+
+            await removeSource(sourceId, title)(dispatch);
+
+            expect(dispatch.mock.calls).toHaveLength(2);
+
+            expect(dispatch.mock.calls[0][0]).toEqual({
+                type: ACTION_TYPES.REMOVE_SOURCE_PENDING,
+                meta: {
+                    sourceId
+                }
+            });
+
+            expect(dispatch.mock.calls[1][0]).toEqual({
+                type: ACTION_TYPES.REMOVE_SOURCE_REJECTED,
+                meta: {
+                    sourceId
+                }
+            });
         });
     });
 });
