041326b869d44d2f524e7967c0193e5709ef6b98
Richard Vsiansky <r.vsia@seznam.cz>
2020-01-06 13:24:08 +0100

    Update tests for addApplication

--- src/test/components/addApplication/AddApplication.spec.js
+++ src/test/components/addApplication/AddApplication.spec.js
@@ -5,11 +5,13 @@ import { Route, MemoryRouter } from 'react-router-dom';
 import { notificationsMiddleware } from '@redhat-cloud-services/frontend-components-notifications';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { CardSelect } from '@redhat-cloud-services/frontend-components-sources';
+import { CardSelect, SummaryStep } from '@redhat-cloud-services/frontend-components-sources';
 import { act } from 'react-dom/test-utils';
 
 import SourcesFormRenderer from '../../../Utilities/SourcesFormRenderer';
 import * as entities from '../../../api/entities';
+import * as attachSource from '../../../api/doAttachApp';
+
 import AddApplication from '../../../components/AddApplication/AddApplication';
 import { componentWrapperIntl } from '../../../Utilities/testsHelpers';
 import { sourceTypesData, OPENSHIFT_ID } from '../../sourceTypesData';
@@ -18,6 +20,8 @@ import { applicationTypesData, COSTMANAGEMENT_APP, TOPOLOGICALINVENTORY_APP } fr
 import AddApplicationDescription from '../../../components/AddApplication/AddApplicationDescription';
 import LoadingStep from '../../../components/steps/LoadingStep';
 import { paths } from '../../../Routes';
+import FinishedStep from '../../../components/steps/FinishedStep';
+import ErroredStepAttach from '../../../components/AddApplication/steps/ErroredStep';
 
 describe('AddApplication', () => {
     let store;
@@ -72,6 +76,60 @@ describe('AddApplication', () => {
         expect(wrapper.find(AddApplicationDescription).length).toEqual(1);
         expect(wrapper.find(CardSelect).length).toEqual(1);
         expect(wrapper.find('Card').length).toEqual(2); // one app is not compatible
+        expect(wrapper.find(Button).at(1).text()).toEqual('Next');
+    });
+
+    it('loads with endpoint values - not removing of source', async () => {
+        const loadAuthsSpy = jest.fn().mockImplementation(() => Promise.resolve({ data: [] }));
+
+        entities.getSourcesApi = () => ({
+            listEndpointAuthentications: loadAuthsSpy
+        });
+
+        const ENDPOINT_ID = '21312';
+
+        store = mockStore({
+            providers: {
+                entities: [{
+                    id: SOURCE_NO_APS_ID,
+                    source_type_id: OPENSHIFT_ID,
+                    applications: [],
+                    endpoints: [
+                        { id: ENDPOINT_ID }
+                    ]
+                }, {
+                    id: SOURCE_ALL_APS_ID,
+                    source_type_id: OPENSHIFT_ID,
+                    applications: [{
+                        application_type_id: COSTMANAGEMENT_APP.id
+                    }, {
+                        application_type_id: TOPOLOGICALINVENTORY_APP.id
+                    }]
+                }],
+                appTypes: applicationTypesData.data,
+                sourceTypes: sourceTypesData.data,
+                appTypesLoaded: true,
+                sourceTypesLoaded: true,
+                loaded: true
+            }
+        });
+
+        let wrapper;
+
+        await act(async () => {
+            wrapper = mount(componentWrapperIntl(
+                <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
+                store,
+                initialEntry
+            ));
+        });
+        wrapper.update();
+
+        expect(loadAuthsSpy).toHaveBeenCalledWith(ENDPOINT_ID);
+        expect(wrapper.find(SourcesFormRenderer).length).toEqual(1);
+        expect(wrapper.find(AddApplicationDescription).length).toEqual(1);
+        expect(wrapper.find(CardSelect).length).toEqual(1);
+        expect(wrapper.find('Card').length).toEqual(2);
         expect(wrapper.find(Button).at(1).text()).toEqual('Next');
     });
 
@@ -146,83 +204,148 @@ describe('AddApplication', () => {
         expect(wrapper.find(MemoryRouter).instance().history.location.pathname).toEqual(paths.sources);
     });
 
-    it.skip('renders review', () => {
-        const wrapper = mount(componentWrapperIntl(
-            <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
-            store,
-            initialEntry
-        ));
-
-        wrapper.find('Card').first().simulate('click');
-        wrapper.find(Button).at(1).simulate('click');
-        wrapper.update();
-
-        expect(wrapper.find(Button).at(1).text()).toEqual('Finish');
-    });
-
-    it.skip('calls on submit function', (done) => {
-        entities.doCreateApplication = jest.fn(() => new Promise((resolve) => resolve('OK')));
-
-        const wrapper = mount(componentWrapperIntl(
-            <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
-            store,
-            initialEntry
-        ));
-
-        wrapper.find('Card').first().simulate('click');
-        wrapper.find(Button).at(1).simulate('click');
-        wrapper.update();
-        wrapper.find(Button).at(1).simulate('click');
-
-        expect(entities.doCreateApplication).toHaveBeenCalledWith(SOURCE_NO_APS_ID, '2');
-        setImmediate(() => {
-            wrapper.update();
-            //expect(wrapper.find(FinishedStep).length).toEqual(1);
-            done();
+    describe('imported source - not need to edit any value', () => {
+        beforeEach(() => {
+            store = mockStore({
+                providers: {
+                    entities: [{
+                        id: SOURCE_NO_APS_ID,
+                        source_type_id: OPENSHIFT_ID,
+                        applications: [],
+                        imported: 'cfme'
+                    }],
+                    appTypes: applicationTypesData.data,
+                    sourceTypes: sourceTypesData.data,
+                    appTypesLoaded: true,
+                    sourceTypesLoaded: true,
+                    loaded: true
+                }
+            });
         });
-    });
 
-    it.skip('catch errors after submit', (done) => {
-        const ERROR_MESSAGE = 'Something went wrong :(';
+        it('renders review', () => {
+            const wrapper = mount(componentWrapperIntl(
+                <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
+                store,
+                initialEntry
+            ));
 
-        entities.doCreateApplication = jest.fn(() => new Promise((_resolve, reject) => reject(
-            { errors: [{ detail: ERROR_MESSAGE }] }
-        )));
-
-        const wrapper = mount(componentWrapperIntl(
-            <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
-            store,
-            initialEntry
-        ));
-
-        wrapper.find('Card').first().simulate('click');
-        wrapper.find(Button).at(1).simulate('click');
-        wrapper.update();
-        wrapper.find(Button).at(1).simulate('click');
-
-        expect(entities.doCreateApplication).toHaveBeenCalledWith(SOURCE_NO_APS_ID, '2');
-        setImmediate(() => {
+            wrapper.find('Card').first().simulate('click');
+            wrapper.find(Button).at(1).simulate('click');
             wrapper.update();
-            //expect(wrapper.find(ErroredStep).length).toEqual(1);
+
+            expect(wrapper.find(SummaryStep)).toHaveLength(1);
+            expect(wrapper.find(Button).at(1).text()).toEqual('Finish');
+        });
+
+        it('calls on submit function', async () => {
+            attachSource.doAttachApp = jest.fn().mockImplementation(() => Promise.resolve('ok'));
+
+            entities.doLoadEntities = jest.fn().mockImplementation(() => Promise.resolve({ sources: [] }));
+            entities.doLoadCountOfSources = jest.fn().mockImplementation(() => Promise.resolve({ meta: { count: 0 } }));
+
+            const wrapper = mount(componentWrapperIntl(
+                <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
+                store,
+                initialEntry
+            ));
+
+            await act(async () => {
+                wrapper.find('Card').first().simulate('click');
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            await act(async () => {
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            await act(async () => {
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            const formValues = { application: {
+                application_type_id: '2'
+            } };
+            const formApi = expect.any(Object);
+            const authenticationValues = expect.any(Array);
+
+            expect(attachSource.doAttachApp).toHaveBeenCalledWith(
+                formValues,
+                formApi,
+                authenticationValues,
+            );
+            expect(wrapper.find(FinishedStep).length).toEqual(1);
+        });
+
+        it('catch errors after submit', async () => {
+            const ERROR_MESSAGE = 'Something went wrong :(';
+
+            attachSource.doAttachApp = jest.fn().mockImplementation(() => new Promise((res, reject) => reject(ERROR_MESSAGE)));
+
+            const wrapper = mount(componentWrapperIntl(
+                <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
+                store,
+                initialEntry
+            ));
+
+            await act(async () => {
+                wrapper.find('Card').first().simulate('click');
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            await act(async () => {
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            await act(async () => {
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            const formValues = { application: {
+                application_type_id: '2'
+            } };
+            const formApi = expect.any(Object);
+            const authenticationValues = expect.any(Array);
+
+            expect(attachSource.doAttachApp).toHaveBeenCalledWith(
+                formValues,
+                formApi,
+                authenticationValues,
+            );
+            expect(wrapper.find(ErroredStepAttach).length).toEqual(1);
             expect(wrapper.find(EmptyStateBody).text().includes(ERROR_MESSAGE)).toEqual(true);
-            done();
         });
-    });
 
-    it.skip('show loading step', () => {
-        entities.doCreateApplication = jest.fn(() => new Promise((resolve) => resolve('OK')));
+        it('show loading step', async () => {
+            attachSource.doAttachApp = jest.fn().mockImplementation(() => Promise.resolve('ok'));
 
-        const wrapper = mount(componentWrapperIntl(
-            <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
-            store,
-            initialEntry
-        ));
+            const wrapper = mount(componentWrapperIntl(
+                <Route path={paths.sourceManageApps} render={ (...args) => <AddApplication { ...args }/> } />,
+                store,
+                initialEntry
+            ));
 
-        wrapper.find('Card').first().simulate('click');
-        wrapper.find(Button).at(1).simulate('click');
-        wrapper.update();
-        wrapper.find(Button).at(1).simulate('click');
-        wrapper.update();
-        expect(wrapper.find(LoadingStep).length).toEqual(1);
+            await act(async () => {
+                wrapper.find('Card').first().simulate('click');
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            await act(async () => {
+                wrapper.find(Button).at(1).simulate('click');
+            });
+            wrapper.update();
+
+            wrapper.find(Button).at(1).simulate('click');
+            wrapper.update();
+
+            expect(wrapper.find(LoadingStep).length).toEqual(1);
+        });
     });
 });
