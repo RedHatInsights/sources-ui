2d10d26d41e9ac1224a335e97688f10546c054e5
Richard Vsiansky <r.vsia@seznam.cz>
2020-01-27 11:24:49 +0100

    Update store spec

--- src/test/utilities/store.spec.js
+++ src/test/utilities/store.spec.js
@@ -1,10 +1,12 @@
 import { getDevStore, getProdStore } from '../../Utilities/store';
 import { defaultSourcesState } from '../../redux/sources/reducer';
+import { defaultUserState } from '../../redux/user/reducer';
 
 describe('store creator', () => {
     const EXPECTED_DEFAULT_STATE = {
         notifications: [],
-        sources: defaultSourcesState
+        sources: defaultSourcesState,
+        user: defaultUserState
     };
 
     it('creates DevStore', () => {
