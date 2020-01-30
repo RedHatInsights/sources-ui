14ee10ffde373a77e06c8bd45423dc0f5398eb7c
Richard Vsiansky <r.vsia@seznam.cz>
2020-01-30 17:13:37 +0100

    Add URL query middleware

--- src/test/utilities/store.spec.js
+++ src/test/utilities/store.spec.js
@@ -1,6 +1,8 @@
-import { getDevStore, getProdStore } from '../../Utilities/store';
+import { getDevStore, getProdStore, urlQueryMiddleware } from '../../Utilities/store';
 import { defaultSourcesState } from '../../redux/sources/reducer';
 import { defaultUserState } from '../../redux/user/reducer';
+import * as queries from '../../Utilities/urlQuery';
+import { ACTION_TYPES } from '../../redux/sources/actions-types';
 
 describe('store creator', () => {
     const EXPECTED_DEFAULT_STATE = {
@@ -19,5 +21,76 @@ describe('store creator', () => {
         const store = getProdStore();
 
         expect(store.getState()).toEqual(EXPECTED_DEFAULT_STATE);
+    });
+
+    describe('url middleware', () => {
+        let store;
+        let next;
+        let action;
+        let sources;
+
+        beforeEach(() => {
+            next = jest.fn();
+            queries.updateQuery = jest.fn();
+        });
+
+        it('calls update url when app is loaded and type is load_entities_fulfilled', () => {
+            sources = {
+                pageSize: 1,
+                pageNumber: 125,
+                loaded: true
+            };
+
+            action = {
+                type: ACTION_TYPES.LOAD_ENTITIES_FULFILLED
+            };
+
+            store = {
+                getState: () => ({ sources })
+            };
+
+            urlQueryMiddleware(store)(next)(action);
+
+            expect(next).toHaveBeenCalledWith(action);
+            expect(queries.updateQuery).toHaveBeenCalledWith(sources);
+        });
+
+        it('does not call update url when app is not loaded and type is load_entities_fulfilled', () => {
+            sources = {
+                loaded: false
+            };
+
+            action = {
+                type: ACTION_TYPES.LOAD_ENTITIES_FULFILLED
+            };
+
+            store = {
+                getState: () => ({ sources })
+            };
+
+            urlQueryMiddleware(store)(next)(action);
+
+            expect(next).toHaveBeenCalledWith(action);
+            expect(queries.updateQuery).not.toHaveBeenCalledWith(sources);
+        });
+
+        it('does not call update url when app is loaded and type is not load_entities_fulfilled', () => {
+            sources = {
+                loaded: false
+            };
+
+            action = {
+                type: ACTION_TYPES.LOAD_ENTITIES_PENDING
+            };
+
+            store = {
+                getState: () => ({ sources })
+            };
+
+            urlQueryMiddleware(store)(next)(action);
+
+            expect(next).toHaveBeenCalledWith(action);
+            expect(queries.updateQuery).not.toHaveBeenCalledWith(sources);
+        });
     });
 });
