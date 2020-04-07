a1a43913a765faa9f5f0e35df313585ebe26591e
Richard Vsiansky <r.vsia@seznam.cz>
2020-04-07 12:29:29 +0200

    Add progress bar to add application wizard
    - also merges add application steps/steps

--- src/components/AddApplication/AddApplication.js
+++ src/components/AddApplication/AddApplication.js
@@ -9,7 +9,7 @@ import { filterApps } from '@redhat-cloud-services/frontend-components-sources';
 import { loadEntities } from '../../redux/sources/actions';
 import SourcesFormRenderer from '../../utilities/SourcesFormRenderer';
 import createSchema from './AddApplicationSchema';
-import LoadingStep from '../steps/LoadingStep';
+import LoadingStep from './steps/LoadingStep';
 import ErroredStep from './steps/ErroredStep';
 import FinishedStep from './steps/FinishedStep';
 import WizardBody from './WizardBody';
@@ -28,18 +28,18 @@ import { checkSourceStatus } from '../../api/checkSourceStatus';
 let selectedApp = undefined; // this has to be not-state value, because it shouldn't re-render the component when changes
 const saveSelectedApp = ({ values: { application } }) => selectedApp = application;
 
-export const onSubmit = (values, formApi, authenticationInitialValues, dispatch, setState, initialValues) => {
-    setState({ state: 'submitting' });
+export const onSubmit = (values, formApi, authenticationInitialValues, dispatch, setState, initialValues, intl) => {
+    setState({ type: 'submit' });
 
-    return doAttachApp(values, formApi, authenticationInitialValues, initialValues)
+    return doAttachApp(values, formApi, authenticationInitialValues, initialValues, setState, intl)
     .then(async () => {
         checkSourceStatus(initialValues.source.id);
         await dispatch(loadEntities());
         selectedApp = undefined;
-        return setState({ state: 'finished' });
+        return setState({ type: 'finish' });
     })
     .catch(error => setState({
-        state: 'errored',
+        type: 'error',
         error,
         values
     }));
@@ -51,10 +51,72 @@ const initialState = {
     values: {},
     authenticationsValues: [],
     sourceAppsLength: 0,
-    initialValues: {}
+    initialValues: {},
+    progressStep: 0,
+    progressTexts: []
 };
 
-const reducer = (state, payload) => ({ ...state, ...payload });
+const reducer = (state, { type, length, authenticationsValues, initialValues, error, values, progressTexts }) => {
+    switch (type) {
+        case 'setSourceAppslength':
+            return {
+                ...state,
+                sourceAppsLength: length
+            };
+        case 'loadAuthentications':
+            return {
+                ...state,
+                authenticationsValues,
+                initialValues,
+                values: {},
+                state: state.state === 'loading' ? 'wizard' : state.state
+            };
+        case 'loadWithoutAuthentications':
+            return {
+                ...state,
+                initialValues,
+                values: {},
+                state: state.state === 'loading' ? 'wizard' : state.state
+            };
+        case 'reset':
+            return {
+                ...state,
+                state: 'wizard'
+            };
+        case 'submit':
+            return {
+                ...state,
+                state: 'submitting',
+                progressStep: 0,
+                progressTexts: ['Preparing']
+            };
+        case 'finish':
+            return {
+                ...state,
+                state: 'finished',
+                progressStep: ++state.progressStep
+            };
+        case 'error':
+            return {
+                ...state,
+                state: 'errored',
+                error,
+                values
+            };
+        case 'setProgressTexts':
+            return {
+                ...state,
+                progressTexts
+            };
+        case 'increaseProgressStep':
+            return {
+                ...state,
+                progressStep: ++state.progressStep
+            };
+        default:
+            return state;
+    }
+};
 
 const AddApplication = () => {
     const intl = useIntl();
@@ -85,13 +147,14 @@ const AddApplication = () => {
             // When app is only removed, there is no need to reload values
             const removeAppAction = state.sourceAppsLength >= source.applications.length && state.sourceAppsLength > 0;
 
-            setState({ sourceAppsLength: source.applications.length });
+            setState({ type: 'setSourceAppslength', length: source.applications.length });
 
             if (!removeAppAction) {
                 if (source.endpoints && source.endpoints[0]) {
                     getSourcesApi()
                     .listEndpointAuthentications(source.endpoints[0].id)
                     .then(({ data }) => setState({
+                        type: 'loadAuthentications',
                         authenticationsValues: data,
                         initialValues: {
                             source,
@@ -100,24 +163,13 @@ const AddApplication = () => {
                             application: selectedApp,
                         },
                         values: {}
-                    }))
-                    .then(() => {
-                        if (state.state === 'loading') {
-                            setState({
-                                state: 'wizard'
-                            });
-                        }
-                    });
+                    }));
                 } else {
                     setState({
+                        type: 'loadWithoutAuthentications',
                         initialValues: { source, application: selectedApp },
                         values: {}
                     });
-                    if (state.state === 'loading') {
-                        setState({
-                            state: 'wizard'
-                        });
-                    }
                 }
             }
         }
@@ -125,7 +177,7 @@ const AddApplication = () => {
 
     const goToSources = () => history.push(routes.sources.path);
 
-    if (!appTypesLoaded || !sourceTypesLoaded || !loaded) {
+    if ((!appTypesLoaded || !sourceTypesLoaded || !loaded || state.state === 'loading') && state.state !== 'submitting') {
         return  (
             <WizardBody
                 goToSources={goToSources}
@@ -134,18 +186,34 @@ const AddApplication = () => {
         );
     }
 
-    if (state.state === 'loading' || state.state === 'submitting') {
+    if (state.state === 'submitting') {
         return  (
             <WizardBody
                 goToSources={goToSources}
-                step={<LoadingStep />}
+                step={<LoadingStep
+                    progressStep={state.progressStep}
+                    progressTexts={state.progressTexts}
+                />}
             />
         );
     }
+
+    const onReset = () => setState({ type: 'reset' });
 
     if (state.state !== 'wizard') {
-        const shownStep = state.state === 'finished' ? (<FinishedStep setState={setState} goToSources={goToSources}/>) :
-            (<ErroredStep setState={setState} goToSources={goToSources} error={state.error}/>);
+        const shownStep = state.state === 'finished' ? (<FinishedStep
+            onReset={onReset}
+            goToSources={goToSources}
+            progressStep={state.progressStep}
+            progressTexts={state.progressTexts}
+        />) :
+            (<ErroredStep
+                onReset={onReset}
+                goToSources={goToSources}
+                error={state.error}
+                progressStep={state.progressStep}
+                progressTexts={state.progressTexts}
+            />);
 
         return (
             <WizardBody goToSources={goToSources} step={shownStep} />
@@ -189,7 +257,8 @@ const AddApplication = () => {
         state.authenticationsValues,
         dispatch,
         setState,
-        state.initialValues
+        state.initialValues,
+        intl
     );
 
     const hasAvailableApps = filteredAppTypes.length > 0;
--- src/components/AddApplication/steps/ErroredStep.js
+++ src/components/AddApplication/steps/ErroredStep.js
@@ -2,36 +2,76 @@ import React from 'react';
 import { FormattedMessage  } from 'react-intl';
 import { PropTypes } from 'prop-types';
 
-import ErroredStep from '../../steps/ErroredStep';
+import { Button } from '@patternfly/react-core/dist/js/components/Button/Button';
+import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
+import { EmptyStateIcon } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateIcon';
+import { EmptyStateBody } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody';
+import { EmptyStateSecondaryActions } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateSecondaryActions';
+import { Bullseye } from '@patternfly/react-core/dist/js/layouts/Bullseye/Bullseye';
+import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
+import { Progress } from '@patternfly/react-core/dist/js/components/Progress/Progress';
+
+import TimesCircleIcon from '@patternfly/react-icons/dist/js/icons/times-circle-icon';
 
 import { Text, TextVariants } from '@patternfly/react-core/dist/js/components/Text/Text';
 import { TextContent } from '@patternfly/react-core/dist/js/components/Text/TextContent';
 
-const ErroredStepAttach = ({ setState, goToSources, error }) => (<ErroredStep
-    onClose={goToSources}
-    message={
-        <React.Fragment>
-            <FormattedMessage
-                id="sources.successAddApp"
-                defaultMessage="Your application has not been successfully added:"
-            />
-            <br />
-            <TextContent>
-                <Text component={TextVariants.h6}>{ error }</Text>
-            </TextContent>
-        </React.Fragment>
-    }
-    title={<FormattedMessage
-        id="sources.configurationSuccessful"
-        defaultMessage="Configuration unsuccessful"
-    />}
-    onRetry={() => setState({ state: 'wizard' })}
-/>);
+const ErroredStepAttach = ({ onReset, goToSources, error, progressStep, progressTexts }) => (
+    <Bullseye>
+        <EmptyState variant={ EmptyStateVariant.full } className="ins-c-sources__empty-state">
+            <EmptyStateIcon icon={ TimesCircleIcon } color="var(--pf-global--danger-color--100)" />
+            <Title headingLevel="h5" size="lg">
+                <FormattedMessage
+                    id="sources.configurationSuccessful"
+                    defaultMessage="Configuration unsuccessful"
+                />
+            </Title>
+            <EmptyStateBody>
+                <Progress
+                    className="pf-u-mb-md ins-c-sources__progress"
+                    value={progressStep}
+                    min={0}
+                    title=" "
+                    max={progressTexts.length - 1}
+                    label={progressTexts[progressStep]}
+                    valueText={progressTexts[progressStep]}
+                    variant='danger'
+                />
+                <React.Fragment>
+                    <FormattedMessage
+                        id="sources.successAddApp"
+                        defaultMessage="Your application has not been successfully added:"
+                    />
+                    <br />
+                    <TextContent>
+                        <Text component={TextVariants.h6}>{ error }</Text>
+                    </TextContent>
+                </React.Fragment>
+            </EmptyStateBody>
+            <Button variant="primary" onClick={ goToSources }>
+                <FormattedMessage
+                    id="sources.backToSources"
+                    defaultMessage="Back to Sources"
+                />
+            </Button>
+            { onReset && <EmptyStateSecondaryActions>
+                <Button variant="link" onClick={ onReset }>
+                    <FormattedMessage
+                        id="sources.retry"
+                        defaultMessage="Retry"
+                    />
+                </Button>
+            </EmptyStateSecondaryActions>}
+        </EmptyState>
+    </Bullseye>
+);
 
 ErroredStepAttach.propTypes = {
-    setState: PropTypes.func.isRequired,
+    onReset: PropTypes.func.isRequired,
     goToSources: PropTypes.func.isRequired,
-    error: PropTypes.string.isRequired
+    error: PropTypes.string.isRequired,
+    progressStep: PropTypes.number.isRequired,
+    progressTexts: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default ErroredStepAttach;
