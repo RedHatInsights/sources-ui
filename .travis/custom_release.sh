9c5bca706ca2e2134ebe0185b0a35066f7a9c79b
Greg McCullough <gmccullo@redhat.com>
2020-07-10 14:27:27 -0400

    Added missing "rm -rf" between calls to release.sh
    Realized that the qa-beta branch for [sources-ui-deploy](https://github.com/RedHatInsights/sources-ui-deploy/tree/qa-beta) repo was 24 days old when it should have been updated with the last merge to master based on the update to this script.
    
    However, we were missing the call to `rm -rf dist/.git` between calls to `.travis/release.sh` which I believe is the root cause.

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -3,10 +3,20 @@
 NODE_ENV=production npm run build
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    echo "PUSHING ci-beta"
+    rm -rf dist/.git
     .travis/release.sh "ci-beta"
+
+    echo "PUSHING qa-beta"
+    rm -rf dist/.git
     .travis/release.sh "qa-beta"
 elif [ "${TRAVIS_BRANCH}" = "master-stable" ]; then
+    echo "PUSHING ci-stable"
+    rm -rf dist/.git
     .travis/release.sh "ci-stable"
+    
+    echo "PUSHING qa-stable"
+    rm -rf dist/.git
     .travis/release.sh "qa-stable"
 elif [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     .travis/release.sh "${TRAVIS_BRANCH}"
