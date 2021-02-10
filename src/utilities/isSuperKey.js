d3de2006f24022c092ce1d788701636dae6088cf
Richard Vsiansky <r.vsia@seznam.cz>
2021-02-10 14:27:40 +0100

    Update super key attribute

--- src/utilities/isSuperKey.js
+++ src/utilities/isSuperKey.js
@@ -1,3 +1,3 @@
-const isSuperKey = (source) => source.app_creation_workflow === 'trust';
+const isSuperKey = (source) => source.app_creation_workflow === 'account_authorization';
 
 export default isSuperKey;
