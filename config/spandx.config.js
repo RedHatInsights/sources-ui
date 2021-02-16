85303fdcacaf3251bfc575b5abc9dc8692d5cb4f
rvsia <r.vsia@seznam.cz>
2021-02-16 16:39:35 +0100

    Update Config dep

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -3,6 +3,7 @@ const localhost = 'localhost';
 module.exports = {
   routes: {
     '/beta/settings/sources': { host: `http://${localhost}:8002` },
+    '/settings/sources': { host: `http://${localhost}:8002` },
     '/beta/apps/sources': { host: `http://${localhost}:8002` },
     '/apps/sources': { host: `http://${localhost}:8002` },
   },
