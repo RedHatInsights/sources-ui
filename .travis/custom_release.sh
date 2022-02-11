5d539e21fc5301117e87ef910e0b36d13aef9ba2
rvsia <r.vsia@seznam.cz>
2022-02-11 11:19:19 +0100

    Automatically push to stage-stable and remove CI

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -4,18 +4,12 @@ set -x
 
 if [ "${TRAVIS_BRANCH}" = "master" ]
 then
-    for env in ci qa
+    for env in qa
     do
         echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
-    done
-fi
 
-if [ "${TRAVIS_BRANCH}" = "stable" ]
-then
-    for env in ci qa
-    do
         echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
