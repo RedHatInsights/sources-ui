87d84f5499b58fec63a581d1d11f720966bc555f
Karel Hala <khala@redhat.com>
2023-04-13 15:25:48 +0200

    Use proper bucket name when creating google cloud, final steps tweak

--- src/components/addSourceWizard/hardcodedSchemas.js
+++ src/components/addSourceWizard/hardcodedSchemas.js
@@ -1487,7 +1487,7 @@ const hardcodedSchemas = {
                 },
                 {
                   component: componentTypes.TEXT_FIELD,
-                  name: 'application.extra.bucket_name',
+                  name: 'application.extra.bucket',
                   label: 'Cloud storage bucket name',
                   validate: [
                     {
