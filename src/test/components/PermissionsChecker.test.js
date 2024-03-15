f3109ae6a7d7605668f8bce2e5225aa8f2f3ab04
Cody Mitchell <comitche@redhat.com>
2024-03-15 13:13:56 -0400

    Allow users with read permissions to view Integrations tabs (#1207)
    * show integrations tabs for users with read permission
    
    * use map in place of reduce

--- src/test/components/PermissionsChecker.test.js
+++ src/test/components/PermissionsChecker.test.js
@@ -9,6 +9,7 @@ describe('PermissionChecker', () => {
     const Children = () => <h1>App</h1>;
     actions.loadWritePermissions = jest.fn().mockImplementation(() => ({ type: 'type' }));
     actions.loadOrgAdmin = jest.fn().mockImplementation(() => ({ type: 'type' }));
+    actions.loadReadIntegrationsEndpointsPermissions = jest.fn().mockImplementation(() => ({ type: 'type' }));
 
     render(
       componentWrapperIntl(
