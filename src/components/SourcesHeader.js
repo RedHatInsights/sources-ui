a50c4612a6ce836398e3aad827272e7ab870d264
Filip Hlavac <filip.hlavac01@gmail.com>
2024-09-18 20:15:22 +0200

    Fix learn more title

--- src/components/SourcesHeader.js
+++ src/components/SourcesHeader.js
@@ -19,12 +19,18 @@ const SourcesHeader = () => {
           <ContentHeader
             ouiaId={'sources-header'}
             title={intl.formatMessage({ id: 'sources.integrations', defaultMessage: 'Integrations' })}
-            subtitle="Integrating third-party applications expands the scope of notifications beyond emails and messages, so that you can view and manage Hybrid Cloud Console events from your preferred platform dashboard."
+            subtitle={intl.formatMessage({
+              id: 'sources.integrations.subtitle',
+              defaultMessage:
+                'Integrating third-party applications expands the scope of notifications beyond emails and messages, so that you can view and manage Hybrid Cloud Console events from your preferred platform dashboard.',
+            })}
             icon={<img src={integrationsIcon} alt="integrations-header-icon" />}
             linkProps={{
-              label: 'Learn more',
+              label: intl.formatMessage({ id: 'integrations.overview.learnMore', defaultMessage: 'Learn more' }),
               isExternal: true,
-              to: 'https://access.redhat.com/documentation/en-us/red_hat_hybrid_cloud_console/1-latest/html/configuring_notifications_on_the_red_hat_hybrid_cloud_console/index',
+              target: '_blank',
+              component: 'a',
+              href: 'https://docs.redhat.com/en/documentation/red_hat_hybrid_cloud_console/1-latest/html/integrating_the_red_hat_hybrid_cloud_console_with_third-party_applications/index',
             }}
             actionMenu={
               <IntegrationsDropdown
