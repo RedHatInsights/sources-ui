10487fbac76fec64d37d5fdf388ce06ca6c4cff1
ewinchel <ewinchel@redhat.com>
2024-10-07 14:19:06 -0400

    styling fixes #1

--- src/components/Widget/IntegrationsWidget.tsx
+++ src/components/Widget/IntegrationsWidget.tsx
@@ -1,5 +1,19 @@
-import { Badge, Card, CardBody, CardFooter, ExpandableSection, List, ListItem, ListVariant, Spinner, Tile} from '@patternfly/react-core';
-import React, { FunctionComponent, useState } from 'react';
+import {
+  Badge,
+  Card,
+  CardBody,
+  CardFooter,
+  ExpandableSection,
+  Gallery,
+  Grid,
+  GridItem,
+  List,
+  ListItem,
+  ListVariant,
+  Spinner,
+  Tile,
+} from '@patternfly/react-core';
+import React, { FunctionComponent, SetStateAction, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import IntegrationsDropdown from '../IntegrationsDropdown';
 import { CLOUD_VENDOR, COMMUNICATIONS, REDHAT_VENDOR, REPORTING, WEBHOOKS } from '../../utilities/constants';
@@ -7,6 +21,8 @@ import { getProdStore } from '../../utilities/store';
 import ImageWithPlaceholder from '../TilesShared/ImageWithPlaceholder';
 import { AsyncComponent } from '@redhat-cloud-services/frontend-components';
 import AddSourceWizard from '../addSourceWizard';
+
+import './IntegrationsWidget.scss';
 
 interface IntegrationItem {
   name: string;
@@ -22,266 +38,289 @@ const integrationsData = [
   {
     title: COMMUNICATIONS,
     items: [
-      { name: 'Google Chat',
+      {
+        name: 'Google Chat',
         id: '0',
         value: COMMUNICATIONS,
-        icon: <ImageWithPlaceholder 
-        className="google-chat-logo" 
-        src="/apps/frontend-assets/sources-integrations/google-chat.svg" 
-        alt="google chat" 
-      />
+        icon: (
+          <ImageWithPlaceholder
+            className="google-chat-logo"
+            src="/apps/frontend-assets/sources-integrations/google-chat.svg"
+            alt="google chat"
+          />
+        ),
       },
-      { name: 'Microsoft Office Teams',
+      {
+        name: 'Microsoft Office Teams',
         id: '1',
         value: COMMUNICATIONS,
-        icon: <ImageWithPlaceholder 
-        className="microsoft-teams-logo" 
-        src="/apps/frontend-assets/sources-integrations/microsoft-office-teams.svg" 
-        alt="microsoft teams" 
-      />
+        icon: (
+          <ImageWithPlaceholder
+            className="microsoft-teams-logo"
+            src="/apps/frontend-assets/sources-integrations/microsoft-office-teams.svg"
+            alt="microsoft teams"
+          />
+        ),
       },
-      { name: 'Slack',
+      {
+        name: 'Slack',
         id: '2',
         value: COMMUNICATIONS,
-        icon: <ImageWithPlaceholder 
-        className="slack-logo" 
-        src="/apps/frontend-assets/sources-integrations/slack.svg" 
-        alt="slack" 
-      />
-      }
-    ]
+        icon: (
+          <ImageWithPlaceholder className="slack-logo" src="/apps/frontend-assets/sources-integrations/slack.svg" alt="slack" />
+        ),
+      },
+    ],
   },
   {
     title: REPORTING,
     items: [
-      { name: 'Event-Driven Ansible',
+      {
+        name: 'Event-Driven Ansible',
         id: '3',
         value: REPORTING,
-        icon: <ImageWithPlaceholder 
-        className="ansible-logo" 
-        src="/apps/frontend-assets/sources-integrations/ansible.svg" 
-        alt="ansible" 
-      />
+        icon: (
+          <ImageWithPlaceholder
+            className="ansible-logo"
+            src="/apps/frontend-assets/sources-integrations/ansible.svg"
+            alt="ansible"
+          />
+        ),
       },
-      { name: 'ServiceNow',
+      {
+        name: 'ServiceNow',
         id: '4',
         value: REPORTING,
-        icon: <ImageWithPlaceholder 
-        className="service-now-logo" 
-        src="/apps/frontend-assets/sources-integrations/service-now.svg" 
-        alt="service" 
-      />
+        icon: (
+          <ImageWithPlaceholder
+            className="service-now-logo"
+            src="/apps/frontend-assets/sources-integrations/service-now.svg"
+            alt="service"
+          />
+        ),
       },
-      { name: 'Splunk',
+      {
+        name: 'Splunk',
         id: '5',
         value: REPORTING,
-        icon: <ImageWithPlaceholder 
-        className="splunk-logo" 
-        src="/apps/frontend-assets/sources-integrations/splunk.svg" 
-        alt="splunk" 
-      />
-      }
-    ]
+        icon: (
+          <ImageWithPlaceholder
+            className="splunk-logo"
+            src="/apps/frontend-assets/sources-integrations/splunk.svg"
+            alt="splunk"
+          />
+        ),
+      },
+    ],
   },
   {
     title: WEBHOOKS,
     items: [
-      { name: 'Webhooks',
+      {
+        name: 'Webhooks',
         id: '6',
         value: WEBHOOKS,
-        icon: <ImageWithPlaceholder 
-        className="webhook-logo" 
-        src="/apps/frontend-assets/integrations-landing/integrations-landing-webhook-icon.svg" 
-        alt="webhooks" 
-      />
-    }
-  ]
+        icon: (
+          <ImageWithPlaceholder
+            className="webhook-logo"
+            src="/apps/frontend-assets/integrations-landing/integrations-landing-webhook-icon.svg"
+            alt="webhooks"
+          />
+        ),
+      },
+    ],
   },
   {
     title: CLOUD_VENDOR,
     items: [
-      { name: 'Amazon Web Services',
+      {
+        name: 'Amazon Web Services',
         id: '7',
         value: CLOUD_VENDOR,
-        icon: <ImageWithPlaceholder
-        className="aws-logo"
-        src="/apps/frontend-assets/partners-icons/aws.svg"
-        alt="aws logo"
-      />
-       },
-      { name: 'Google Cloud',
+        icon: <ImageWithPlaceholder className="aws-logo" src="/apps/frontend-assets/partners-icons/aws.svg" alt="aws logo" />,
+      },
+      {
+        name: 'Google Cloud',
         id: '8',
         value: CLOUD_VENDOR,
-        icon: <ImageWithPlaceholder
-        className="google-logo"
-        src="/apps/frontend-assets/partners-icons/google-cloud-short.svg"
-        alt="google logo"
-      />
-       },
-      { name: 'Microsoft Azure',
+        icon: (
+          <ImageWithPlaceholder
+            className="google-logo"
+            src="/apps/frontend-assets/partners-icons/google-cloud-short.svg"
+            alt="google logo"
+          />
+        ),
+      },
+      {
+        name: 'Microsoft Azure',
         id: '9',
         value: CLOUD_VENDOR,
-        icon: <ImageWithPlaceholder
-        className="azure-logo"
-        src="/apps/frontend-assets/partners-icons/microsoft-azure-short.svg"
-        alt="azure logo"
-      />
-       },
-      { name: 'Oracle Cloud Infrastructure',
+        icon: (
+          <ImageWithPlaceholder
+            className="azure-logo"
+            src="/apps/frontend-assets/partners-icons/microsoft-azure-short.svg"
+            alt="azure logo"
+          />
+        ),
+      },
+      {
+        name: 'Oracle Cloud Infrastructure',
         id: '10',
         value: CLOUD_VENDOR,
-        icon: <ImageWithPlaceholder
-        className="oracle-logo"
-        src="/apps/frontend-assets/partners-icons/oracle-short.svg"
-        alt="oracle logo"
-      />
-      }
-    ]
+        icon: (
+          <ImageWithPlaceholder
+            className="oracle-logo"
+            src="/apps/frontend-assets/partners-icons/oracle-short.svg"
+            alt="oracle logo"
+          />
+        ),
+      },
+    ],
   },
   {
     title: REDHAT_VENDOR,
     items: [
-      { name: 'OpenShift Container Platform',
+      {
+        name: 'OpenShift Container Platform',
         id: '11',
         value: REDHAT_VENDOR,
-        icon: <ImageWithPlaceholder 
-        className="redhat-icon" 
-        src="/apps/frontend-assets/red-hat-logos/stacked.svg" 
-        alt="red hat logo" 
-      />
-      }
-    ]
-  }
+        icon: (
+          <ImageWithPlaceholder
+            className="redhat-icon"
+            src="/apps/frontend-assets/red-hat-logos/stacked.svg"
+            alt="red hat logo"
+          />
+        ),
+      },
+    ],
+  },
 ];
 
 const IntegrationsWidget: FunctionComponent = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [expandedIndex, setExpandedIndex] = useState<number | null>(0);
-    const [integrations, setIntegrations] = useState<Integration[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(0);
+  const [integrations, setIntegrations] = useState<Integration[]>([]);
 
-    const [selectedTileValue, setSelectedTileValue] = useState<string>('');
-    const [isIntegrationsWizardOpen, setIsIntegrationsWizardOpen] = useState(false);
-    const [isSourcesWizardOpen, setIsSourcesWizardOpen] = useState(false);
+  const [selectedTileValue, setSelectedTileValue] = useState<string | null>(null);
+  const [isWizardOpen, setIsWizardOpen] = useState(false);
 
-    const handleTileClick = (value: string) => {
-      setSelectedTileValue(value);
-      if ([REDHAT_VENDOR, CLOUD_VENDOR].includes(value)) {
-        setIsSourcesWizardOpen(true);
-      } else if ([COMMUNICATIONS, REPORTING, WEBHOOKS].includes(value)) {
-        setIsIntegrationsWizardOpen(true);
-      }
-    };
+  const handleTileClick = (value: string) => {
+    setSelectedTileValue(value);
+    setIsWizardOpen(true);
+  };
 
-    const allItems = integrationsData.flatMap((category) => category.items);
-    const sortedItems = allItems.sort((a, b) => a.name.localeCompare(b.name));
+  const allItems = integrationsData.flatMap((category) => category.items);
+  const sortedItems = allItems.sort((a, b) => a.name.localeCompare(b.name));
 
-    const onToggle = (index: number) => {
-      setExpandedIndex(expandedIndex === index ? null : index);
-    };
+  const onToggle = (index: number) => {
+    setExpandedIndex(expandedIndex === index ? null : index);
+  };
 
-    // const fetchIntegrations = async () => {
-    //   try {
-    //     const response = await fetch(
-    //       '/api/integrations/v1.0/endpoints?type=ansible&type=webhook&type=camel'
-    //     );
-    //     const { data } = await response.json();
-    //     console.log(data);
-    //     setIsLoading(false);
-    //     setIntegrations(data)
-    //   } catch (error) {
-    //     console.error('Unable to get Integrations ', error);
-    //   }
-    // };
-  
-    // useEffect(() => {
-    //   setIsLoading(true);
-    //   fetchIntegrations();
-    // }, []);
+  const fetchIntegrations = async () => {
+    try {
+      const response = await fetch('/api/integrations/v1.0/endpoints?type=ansible&type=webhook&type=camel');
+      const { data } = await response.json();
+      console.log(data);
+      setIsLoading(false);
+      setIntegrations(data);
+    } catch (error) {
+      console.error('Unable to get Integrations ', error);
+    }
+  };
 
-    const integrationCount = integrations.length
+  useEffect(() => {
+    setIsLoading(true);
+    fetchIntegrations();
+  }, []);
+
+  const integrationCount = integrationsData.length;
 
   return (
     <>
-    {isLoading ? (
-      <Spinner />
-    ) :
-    integrationsData.length === 5 ? (
-      <>
-      <Card isPlain ouiaId="integrations-widget-empty-state" isClickable>
+      {isLoading ? (
+        <Spinner />
+      ) : integrationsData.length === 0 ? (
+        <>
+          <Card isPlain ouiaId="integrations-widget-empty-state" isClickable>
+            <CardBody>
+              Click on a third-party application to create an integration for it.{' '}
+              <a href="/settings/integrations?category=overview">Learn more about Integrations.</a>
+            </CardBody>
+            <CardBody>
+              <Gallery hasGutter>
+                {sortedItems.map((item) => (
+                  <Tile
+                    title={item.name}
+                    id={item.id}
+                    icon={item.icon}
+                    isStacked
+                    onClick={() => handleTileClick(item.value)}
+                    isSelected={selectedTileValue === item.id}
+                  />
+                ))}
+              </Gallery>
+            </CardBody>
+            {selectedTileValue === 'COMMUNICATIONS' || 'REPORTING' || 'WEBHOOKS' ? (
+              <AsyncComponent
+                appName="notifications"
+                module="./IntegrationsWizard"
+                isOpen={isWizardOpen}
+                category={selectedTileValue}
+                closeModal={() => {
+                  setIsWizardOpen(false);
+                  setSelectedTileValue(null);
+                }}
+                fallback={<div id="fallback-modal" />}
+              />
+            ) : (
+              <AddSourceWizard
+                isOpen={isWizardOpen}
+                onClose={() => {
+                  setIsWizardOpen(false);
+                  setSelectedTileValue(null);
+                }}
+                activeCategory={selectedTileValue}
+              />
+            )}
+          </Card>
+        </>
+      ) : (
+        <Card ouiaId="integrations-widget" isFullHeight>
           <CardBody>
-          Click on a third-party application to create an integration for it. <a href="/settings/integrations?category=overview">Learn more about Integrations.</a>
-         </CardBody>
-          <CardBody>
-          {sortedItems.map((item) => (
-              <Tile 
-              title={item.name} 
-              id={item.id} 
-              icon={item.icon}
-              isStacked
-              onClick={() => handleTileClick(item.value)}
-              isSelected={selectedTileValue === item.value} />
+            {integrationsData.map((integration, integrationIndex) => (
+              <ExpandableSection
+                key={integrationIndex}
+                toggleContent={
+                  <div>
+                    <span>{integration.title} </span>
+                    <Badge isRead>{integrationCount}</Badge>
+                  </div>
+                }
+                onToggle={() => onToggle(integrationIndex)}
+                isExpanded={expandedIndex === integrationIndex}
+              >
+                <List variant={ListVariant.inline} className="pf-v5-u-mb-md">
+                  {integration.items.map((item, itemIndex) => (
+                    <ListItem key={itemIndex} icon={item.icon}>
+                      {item.name} ()
+                    </ListItem>
+                  ))}
+                </List>
+              </ExpandableSection>
             ))}
           </CardBody>
-          {[COMMUNICATIONS, REPORTING, WEBHOOKS].includes(selectedTileValue) && (
-        <AsyncComponent
-          appName="notifications"
-          module="./IntegrationsWizard"
-          isOpen={isIntegrationsWizardOpen}
-          category={selectedTileValue}
-          closeModal={() => {
-            setIsIntegrationsWizardOpen(false);
-            setSelectedTileValue('');
-          }}
-          fallback={<div id="fallback-modal" />}
-        />
-      )}
-      {[REDHAT_VENDOR, CLOUD_VENDOR].includes(selectedTileValue) && (
-        <AddSourceWizard
-          isOpen={isSourcesWizardOpen}
-          onClose={() => {
-            setIsSourcesWizardOpen(false);
-            setSelectedTileValue('');
-          }}
-          activeCategory={selectedTileValue}
-        />
-      )}
-     </Card> 
-   </>
-     ) : (
-      <Card ouiaId='integrations-widget' isFullHeight>
-        <CardBody>
-        {integrationsData.map((integration, integrationIndex) => 
-<ExpandableSection
-key={integrationIndex}
-      toggleContent={
-          <div>
-            <span>{integration.title} </span>
-            <Badge isRead>{integrationCount}</Badge>
-          </div>
-      }
-      onToggle={() => onToggle(integrationIndex)}
-      isExpanded={expandedIndex === integrationIndex}
-    >
-        {integration.items.map((item, itemIndex) => (
-          <List variant={ListVariant.inline} key={itemIndex} className='pf-v5-u-mb-md'>
-            <ListItem icon={item.icon}>
-              {item.name} ()
-            </ListItem>
-         </List>
-        ))}
-    </ExpandableSection>
-  )}
-    </CardBody>
-    <CardFooter>
-      <IntegrationsDropdown />
-    </CardFooter>
-    </Card>
-     )} </> 
+          <CardFooter>
+            <IntegrationsDropdown />
+          </CardFooter>
+        </Card>
+      )}{' '}
+    </>
   );
 };
 
-const IntegrationsWidgetWrapper = () => <Provider store={getProdStore()}>
+const IntegrationsWidgetWrapper = () => (
+  (<Provider store={getProdStore()}>
   <IntegrationsWidget />
-</Provider>
+</Provider>)
 
 export default IntegrationsWidgetWrapper;
