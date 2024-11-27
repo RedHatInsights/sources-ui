83de1c3f075bca4192b8d0edded9b05fca998e72
Cody Mitchell <comitche@redhat.com>
2024-11-27 10:31:53 -0500

    fix: show tiles in empty state with perms

--- src/components/Widget/consts/widgetData.tsx
+++ src/components/Widget/consts/widgetData.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { COMMUNICATIONS, REPORTING, WEBHOOKS, CLOUD_VENDOR, REDHAT_VENDOR } from "../../../utilities/constants";
-import ImageWithPlaceholder from "../../TilesShared/ImageWithPlaceholder";
+import React from 'react';
+import { COMMUNICATIONS, REPORTING, WEBHOOKS, CLOUD_VENDOR, REDHAT_VENDOR } from '../../../utilities/constants';
+import ImageWithPlaceholder from '../../TilesShared/ImageWithPlaceholder';
 
 type IntegrationItem = {
   name: string;
@@ -14,178 +14,198 @@ type IntegrationCategory = {
   items: IntegrationItem[];
 };
 
-export const createIntegrationsData = (isPagerDutyEnabled: boolean): IntegrationCategory[] => [
-    {
-      title: COMMUNICATIONS,
-      items: [
+export const createIntegrationsData = (
+  isPagerDutyEnabled: boolean,
+  hasSourcesPermissions: boolean,
+  hasIntegrationsPermissions: boolean,
+): IntegrationCategory[] => [
+  ...(hasIntegrationsPermissions
+    ? [
         {
-          name: 'Google Chat',
-          id: 'google_chat',
-          value: COMMUNICATIONS,
-          icon: (
-            <ImageWithPlaceholder
-              className="google-chat-logo"
-              src="/apps/frontend-assets/sources-integrations/google-chat.svg"
-              alt="google chat"
-            />
-          ),
+          title: COMMUNICATIONS,
+          items: [
+            {
+              name: 'Google Chat',
+              id: 'google_chat',
+              value: COMMUNICATIONS,
+              icon: (
+                <ImageWithPlaceholder
+                  className="google-chat-logo"
+                  src="/apps/frontend-assets/sources-integrations/google-chat.svg"
+                  alt="google chat"
+                />
+              ),
+            },
+            {
+              name: 'Microsoft Office Teams',
+              id: 'teams',
+              value: COMMUNICATIONS,
+              icon: (
+                <ImageWithPlaceholder
+                  className="microsoft-teams-logo"
+                  src="/apps/frontend-assets/sources-integrations/microsoft-office-teams.svg"
+                  alt="microsoft teams"
+                />
+              ),
+            },
+            {
+              name: 'Slack',
+              id: 'slack',
+              value: COMMUNICATIONS,
+              icon: (
+                <ImageWithPlaceholder
+                  className="slack-logo"
+                  src="/apps/frontend-assets/sources-integrations/slack.svg"
+                  alt="slack"
+                />
+              ),
+            },
+          ],
         },
         {
-          name: 'Microsoft Office Teams',
-          id: 'teams',
-          value: COMMUNICATIONS,
-          icon: (
-            <ImageWithPlaceholder
-              className="microsoft-teams-logo"
-              src="/apps/frontend-assets/sources-integrations/microsoft-office-teams.svg"
-              alt="microsoft teams"
-            />
-          ),
+          title: REPORTING,
+          items: [
+            {
+              name: 'Event-Driven Ansible',
+              id: 'ansible',
+              value: REPORTING,
+              icon: (
+                <ImageWithPlaceholder
+                  className="ansible-logo"
+                  src="/apps/frontend-assets/sources-integrations/ansible.svg"
+                  alt="ansible"
+                />
+              ),
+            },
+            ...(isPagerDutyEnabled
+              ? [
+                  {
+                    name: 'PagerDuty',
+                    id: 'pagerduty',
+                    value: REPORTING,
+                    icon: (
+                      <ImageWithPlaceholder
+                        className="pagerduty-logo"
+                        src="/apps/frontend-assets/sources-integrations/pagerduty-widget.svg"
+                        alt="pagerduty"
+                      />
+                    ),
+                  },
+                ]
+              : []),
+            {
+              name: 'ServiceNow',
+              id: 'servicenow',
+              value: REPORTING,
+              icon: (
+                <ImageWithPlaceholder
+                  className="service-now-logo"
+                  src="/apps/frontend-assets/sources-integrations/service-now.svg"
+                  alt="service"
+                />
+              ),
+            },
+            {
+              name: 'Splunk',
+              id: 'splunk',
+              value: REPORTING,
+              icon: (
+                <ImageWithPlaceholder
+                  className="splunk-logo"
+                  src="/apps/frontend-assets/sources-integrations/splunk.svg"
+                  alt="splunk"
+                />
+              ),
+            },
+          ],
         },
         {
-          name: 'Slack',
-          id: 'slack',
-          value: COMMUNICATIONS,
-          icon: (
-            <ImageWithPlaceholder className="slack-logo" src="/apps/frontend-assets/sources-integrations/slack.svg" alt="slack" />
-          ),
+          title: WEBHOOKS,
+          items: [
+            {
+              name: 'Webhooks',
+              id: 'webhook',
+              value: WEBHOOKS,
+              icon: (
+                <ImageWithPlaceholder
+                  className="webhook-logo"
+                  src="/apps/frontend-assets/integrations-landing/integrations-landing-webhook-icon.svg"
+                  alt="webhooks"
+                />
+              ),
+            },
+          ],
         },
-      ],
-    },
-    {
-      title: REPORTING,
-      items: [
+      ]
+    : []),
+  ...(hasSourcesPermissions
+    ? [
         {
-          name: 'Event-Driven Ansible',
-          id: 'ansible',
-          value: REPORTING,
-          icon: (
-            <ImageWithPlaceholder
-              className="ansible-logo"
-              src="/apps/frontend-assets/sources-integrations/ansible.svg"
-              alt="ansible"
-            />
-          ),
-        },
-        ...(isPagerDutyEnabled ? 
-        [{
-          name: 'PagerDuty',
-          id: 'pagerduty',
-          value: REPORTING,
-          icon: (
-            <ImageWithPlaceholder
-              className="pagerduty-logo"
-              src="/apps/frontend-assets/sources-integrations/pagerduty-widget.svg"
-              alt="pagerduty"
-            />
-          ),
-        }] : []),
-        {
-          name: 'ServiceNow',
-          id: 'servicenow',
-          value: REPORTING,
-          icon: (
-            <ImageWithPlaceholder
-              className="service-now-logo"
-              src="/apps/frontend-assets/sources-integrations/service-now.svg"
-              alt="service"
-            />
-          ),
-        },
-        {
-          name: 'Splunk',
-          id: 'splunk',
-          value: REPORTING,
-          icon: (
-            <ImageWithPlaceholder
-              className="splunk-logo"
-              src="/apps/frontend-assets/sources-integrations/splunk.svg"
-              alt="splunk"
-            />
-          ),
-        },
-      ],
-    },
-    {
-      title: WEBHOOKS,
-      items: [
-        {
-          name: 'Webhooks',
-          id: 'webhook',
-          value: WEBHOOKS,
-          icon: (
-            <ImageWithPlaceholder
-              className="webhook-logo"
-              src="/apps/frontend-assets/integrations-landing/integrations-landing-webhook-icon.svg"
-              alt="webhooks"
-            />
-          ),
-        },
-      ],
-    },
-    {
-      title: CLOUD_VENDOR,
-      items: [
-        {
-          name: 'Amazon Web Services',
-          id: 'aws',
-          value: CLOUD_VENDOR,
-          icon: <ImageWithPlaceholder className="aws-logo" src="/apps/frontend-assets/partners-icons/aws.svg" alt="aws logo" />,
+          title: CLOUD_VENDOR,
+          items: [
+            {
+              name: 'Amazon Web Services',
+              id: 'aws',
+              value: CLOUD_VENDOR,
+              icon: (
+                <ImageWithPlaceholder className="aws-logo" src="/apps/frontend-assets/partners-icons/aws.svg" alt="aws logo" />
+              ),
+            },
+            {
+              name: 'Google Cloud',
+              id: 'google_cloud',
+              value: CLOUD_VENDOR,
+              icon: (
+                <ImageWithPlaceholder
+                  className="google-logo"
+                  src="/apps/frontend-assets/partners-icons/google-cloud-short.svg"
+                  alt="google logo"
+                />
+              ),
+            },
+            {
+              name: 'Microsoft Azure',
+              id: 'azure',
+              value: CLOUD_VENDOR,
+              icon: (
+                <ImageWithPlaceholder
+                  className="azure-logo"
+                  src="/apps/frontend-assets/partners-icons/microsoft-azure-short.svg"
+                  alt="azure logo"
+                />
+              ),
+            },
+            {
+              name: 'Oracle Cloud Infrastructure',
+              id: 'oracle_cloud',
+              value: CLOUD_VENDOR,
+              icon: (
+                <ImageWithPlaceholder
+                  className="oracle-logo"
+                  src="/apps/frontend-assets/partners-icons/oracle-short.svg"
+                  alt="oracle logo"
+                />
+              ),
+            },
+          ],
         },
         {
-          name: 'Google Cloud',
-          id: 'google_cloud',
-          value: CLOUD_VENDOR,
-          icon: (
-            <ImageWithPlaceholder
-              className="google-logo"
-              src="/apps/frontend-assets/partners-icons/google-cloud-short.svg"
-              alt="google logo"
-            />
-          ),
+          title: REDHAT_VENDOR,
+          items: [
+            {
+              name: 'OpenShift Container Platform',
+              id: 'openshift',
+              value: REDHAT_VENDOR,
+              icon: (
+                <ImageWithPlaceholder
+                  className="redhat-icon"
+                  src="/apps/frontend-assets/service-dropdown-icons/openshift.svg"
+                  alt="red hat logo"
+                />
+              ),
+            },
+          ],
         },
-        {
-          name: 'Microsoft Azure',
-          id: 'azure',
-          value: CLOUD_VENDOR,
-          icon: (
-            <ImageWithPlaceholder
-              className="azure-logo"
-              src="/apps/frontend-assets/partners-icons/microsoft-azure-short.svg"
-              alt="azure logo"
-            />
-          ),
-        },
-        {
-          name: 'Oracle Cloud Infrastructure',
-          id: 'oracle_cloud',
-          value: CLOUD_VENDOR,
-          icon: (
-            <ImageWithPlaceholder
-              className="oracle-logo"
-              src="/apps/frontend-assets/partners-icons/oracle-short.svg"
-              alt="oracle logo"
-            />
-          ),
-        },
-      ],
-    },
-    {
-      title: REDHAT_VENDOR,
-      items: [
-        {
-          name: 'OpenShift Container Platform',
-          id: 'openshift',
-          value: REDHAT_VENDOR,
-          icon: (
-            <ImageWithPlaceholder
-              className="redhat-icon"
-              src="/apps/frontend-assets/service-dropdown-icons/openshift.svg"
-              alt="red hat logo"
-            />
-          ),
-        },
-      ],
-    },
-  ];
-  
+      ]
+    : []),
+];
