ccaf48de240a9339aba9d50b212e46dcba13e7ec
alex <aferdina@redhat.com>
2025-05-22 13:48:36 -0400

    get modal to open from new dropdown

--- src/components/Widget/IntegrationsWidget.tsx
+++ src/components/Widget/IntegrationsWidget.tsx
@@ -14,7 +14,6 @@ import {
   Dropdown,
   DropdownItem,
   Flex,
-  FlexItem,
   Gallery,
   Icon,
   Spinner,
@@ -56,11 +55,11 @@ const IntegrationsWidget: FunctionComponent = () => {
 
   const handleTileClick = (value: string) => {
     setSelectedTileValue(value);
-        if ([REDHAT_VENDOR, CLOUD_VENDOR].includes(value)) {
-        setIsSourcesWizardOpen(true);
-      } else if ([COMMUNICATIONS, REPORTING, WEBHOOKS].includes(value)) {
-        setIsIntegrationsWizardOpen(true);
-      }
+    if ([REDHAT_VENDOR, CLOUD_VENDOR].includes(value)) {
+      setIsSourcesWizardOpen(true);
+    } else if ([COMMUNICATIONS, REPORTING, WEBHOOKS].includes(value)) {
+      setIsIntegrationsWizardOpen(true);
+    }
   };
 
   const allItems = integrationsData.flatMap((category) => category.items);
@@ -69,7 +68,7 @@ const IntegrationsWidget: FunctionComponent = () => {
   useEffect(() => {
     const loadAllSources = async () => {
       setIsLoading(true);
-      
+
       const [cloudResult, redHatResult, integrationResult] = await Promise.all([
         fetchCloudSources(),
         fetchRedHatSources(),
@@ -124,8 +123,13 @@ const IntegrationsWidget: FunctionComponent = () => {
 
   const handleDropdownAction = (action: string, integrationTitle: string) => {
     if (action === 'create') {
-      handleTileClick(integrationTitle);
-
+      if ([REDHAT_VENDOR, CLOUD_VENDOR].includes(integrationTitle)) {
+        setSelectedTileValue(integrationTitle);
+        setIsSourcesWizardOpen(true);
+      } else if ([COMMUNICATIONS, REPORTING, WEBHOOKS].includes(integrationTitle)) {
+        setSelectedTileValue(integrationTitle);
+        setIsIntegrationsWizardOpen(true);
+      }
     } else if (action === 'view') {
       navigate(`/settings/integrations?category=${integrationTitle}`);
     }
@@ -156,7 +160,102 @@ const IntegrationsWidget: FunctionComponent = () => {
                 ))}
               </Gallery>
             </CardBody>
-            {[COMMUNICATIONS, REPORTING, WEBHOOKS].includes(selectedTileValue) && (
+          </Card>
+        </>
+      ) : (
+        <Card ouiaId="integrations-widget" isFullHeight>
+          <CardBody className="pf-v5-u-pt-0">
+            <DataList aria-label="Integrations List" isCompact>
+              {integrationsData.map((integration, integrationIndex) => {
+                const isExpandable = integration.title !== 'Webhooks';
+                return (
+                  <React.Fragment key={`integration-fragment-${integrationIndex}`}>
+                    <DataListItem
+                      key={integrationIndex}
+                      aria-labelledby={`integration-${integrationIndex}`}
+                      isExpanded={isExpandable ? expandedIndex.includes(integrationIndex) : false}
+                    >
+                      <DataListItemRow>
+                        {isExpandable ? (
+                          <DataListToggle
+                            onClick={() => onToggle(integrationIndex, isExpandable)}
+                            isExpanded={expandedIndex.includes(integrationIndex)}
+                            id={`toggle-${integrationIndex}`}
+                            aria-controls={`expand-${integrationIndex}`}
+                          />
+                        ) : (
+                          <div style={{ width: '47px' }} />
+                        )}
+                        <DataListItemCells
+                          dataListCells={[
+                            <DataListCell key="primary content">
+                              <span className="pf-v5-u-pr-sm pf-v6-u-text-color-link pf-v6-u-font-weight-bold">{integration.title}</span>
+                              <Badge isRead={badgeCounts(integration.items) === 0}>
+                                {badgeCounts(integration.items)}
+                              </Badge>
+                            </DataListCell>
+                          ]}
+                        />
+                        <DataListAction aria-labelledby="actions" id="actions" aria-label="Actions">
+                          <Dropdown
+                            onSelect={() => onToggleDropdown(integrationIndex, false)}
+                            toggle={(toggleRef) => (
+                              <DropdownToggle
+                                onToggle={() =>
+                                  onToggleDropdown(integrationIndex, !dropdownOpenIndexes[integrationIndex])
+                                }
+                                id={`dropdown-toggle-${integrationIndex}`}
+                                ref={toggleRef}
+                              >
+                                Manage
+                              </DropdownToggle>
+                            )}
+                            isOpen={dropdownOpenIndexes[integrationIndex] || false}
+                          >
+                            <DropdownItem
+                              key={`create-new-${integrationIndex}`}
+                              onClick={() => {
+                                handleDropdownAction('create', integration.title)}}
+                              isSelected={selectedTileValue === integration.title}
+                            >
+                              Create new {integration.title} integration
+                            </DropdownItem>
+
+                            <DropdownItem
+                              key={`view-all-${integrationIndex}`}
+                              onClick={() => {
+                                handleDropdownAction('view', integration.title);
+                            }} 
+                            >
+                              View all {integration.title} Integrations
+                            </DropdownItem>
+                          </Dropdown>
+                        </DataListAction>
+                      </DataListItemRow>
+                      <DataListContent aria-label="widget-content" id={`expand-${integrationIndex}`}>
+                        {expandedIndex.includes(integrationIndex) && (
+                          <div>
+                            {integration.items.filter((item) => item.name !== 'Webhooks').map((item, itemIndex) => (
+                              <Flex display={{ default: 'inlineFlex' }} className="pf-v5-u-p-sm pf-v6-u-align-items-center">
+                                <Icon size="md" iconSize="lg" className="pf-v5-u-mr-sm" isInline>{item.icon}</Icon>
+                                {item.name} ({integrationCounts[item.id] || 0})
+                              </Flex>
+                            ))}
+                          </div>
+                        )}
+                      </DataListContent>
+                    </DataListItem>
+                  </React.Fragment>
+                );
+              })}
+            </DataList>
+          </CardBody>
+          <CardFooter className="pf-v5-u-pt-md pf-v5-u-background-color-100">
+            <IntegrationsDropdown />
+          </CardFooter>
+        </Card>
+      )}
+        {[COMMUNICATIONS, REPORTING, WEBHOOKS].includes(selectedTileValue) && (
               <AsyncComponent
                 appName="notifications"
                 module="./IntegrationsWizard"
@@ -180,111 +279,14 @@ const IntegrationsWidget: FunctionComponent = () => {
                 activeCategory={selectedTileValue}
               />
             )}
-          </Card>
-        </>
-      ) : (
-        <Card ouiaId="integrations-widget" isFullHeight>
-          <CardBody>
-          <DataList aria-label="Integrations List" isCompact>
-              {integrationsData.map((integration, integrationIndex) => {
-                const isExpandable = integration.title !== 'Webhooks';
-                return (
-                  <React.Fragment key={`integration-fragment-${integrationIndex}`}>
-                    <DataListItem
-                      key={integrationIndex}
-                      aria-labelledby={`integration-${integrationIndex}`}
-                      isExpanded={isExpandable ? expandedIndex.includes(integrationIndex) : false}
-                    >
-                      <DataListItemRow>
-                        {isExpandable ? (
-                          <DataListToggle
-                            onClick={() => onToggle(integrationIndex, isExpandable)}
-                            isExpanded={expandedIndex.includes(integrationIndex)}
-                            id={`toggle-${integrationIndex}`}
-                            aria-controls={`expand-${integrationIndex}`}
-                          />
-                        ) : (
-                          <div style={{ width: '40px' }} />
-                        )}
-                        <DataListItemCells
-                dataListCells={[
-                  <DataListCell key="primary content">
-                      <span className="pf-v5-u-pr-sm">{integration.title}</span>
-                      <Badge isRead={badgeCounts(integration.items) === 0}>
-                        {badgeCounts(integration.items)}
-                      </Badge>
-                  </DataListCell>
-                ]}
-              />
-              <DataListAction aria-labelledby="actions" id="actions" aria-label="Actions">
-                <Dropdown
-                  onSelect={() => onToggleDropdown(integrationIndex, false)}
-                  toggle={(toggleRef) => (
-                    <DropdownToggle
-                      onToggle={() =>
-                        onToggleDropdown(integrationIndex, !dropdownOpenIndexes[integrationIndex])
-                      }
-                      id={`dropdown-toggle-${integrationIndex}`}
-                      ref={toggleRef}
-                    >
-                      Manage
-                    </DropdownToggle>
-                  )}
-                  isOpen={dropdownOpenIndexes[integrationIndex] || false}
-                >
-                  <DropdownItem
-      key={`create-new-${integrationIndex}`}
-      onClick={() => handleDropdownAction('create', integration.title)}
-      isSelected={selectedTileValue === integration.title}
-    >
-      Create new {integration.title} integration
-    </DropdownItem>
-
-    <DropdownItem
-      key={`view-all-${integrationIndex}`}
-      onClick={() => handleDropdownAction('view', integration.title)}    
-      >
-      View all {integration.title} Integrations
-    </DropdownItem>
-                </Dropdown>
-              </DataListAction>
-                      </DataListItemRow>
-            <DataListContent aria-label="widget-content" id={`expand-${integrationIndex}`}>
-              {expandedIndex.includes(integrationIndex) && (
-                <div>
-                  {integration.items.filter((item) => item.name !== 'Webhooks').map((item, itemIndex) => (
-                      <Flex display={{ default: 'inlineFlex' }}
-                            className="pf-v5-u-p-md">
-                    <FlexItem>
-                      <Icon size="lg" className="pf-v5-u-mr-sm">{item.icon}</Icon>
-                      </FlexItem>
-                      <FlexItem>
-                        {item.name} ({integrationCounts[item.id] || 0})
-                      </FlexItem>
-                      </Flex>
-                  ))}
-                </div>
-              )}
-            </DataListContent>
-                    </DataListItem>
-                  </React.Fragment>
-                );
-              })}
-            </DataList>
-          </CardBody>
-          <CardFooter className="pf-v5-u-pt-md pf-v5-u-background-color-100">
-            <IntegrationsDropdown />
-          </CardFooter>
-        </Card>
-      )}{' '}
     </PermissionsChecker>
   );
 };
 
 const IntegrationsWidgetWrapper = () => (
-  (<Provider store={getProdStore()}>
-  <IntegrationsWidget />
-</Provider>)
+  <Provider store={getProdStore()}>
+    <IntegrationsWidget />
+  </Provider>
 );
 
 export default IntegrationsWidgetWrapper;
